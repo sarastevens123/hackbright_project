23c20f818cf2826454fb3b86456cc2301a39b21a
Sara Stevens <sarastevens@Saras-MacBook-Air.local>
2022-11-20 20:22:52 -0800

    updated login

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
 """Server for user/restaurants ratings app."""
 
-from flask import (Flask, render_template, request, flash, session, flask_login,
+from flask import (Flask, render_template, request, flash, session,
                     redirect)
 from model import connect_to_db, db, UserRating, User, Restaurant, RestaurantRating
 import crud
 from jinja2 import StrictUndefined
 
-from flask_login import LoginManager
-login_manager = LoginManager()
 
-login_manager.init_app(app)
 
 
 app = Flask(__name__)
