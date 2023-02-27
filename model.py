0faa502367b435f1a103cda37ec3763a466441db
Sara Stevens <sarastevens123@gmail.com>
2023-02-27 15:37:38 -0800

    added CSS screen resizing

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ class UserRating(db.Model):
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///kickback"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///kickback_database"
     app.config["SQLALCHEMY_ECHO"] = False
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
